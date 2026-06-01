18c90b1c1c8407762af67a000719f867aa64abd0
Nightcore <Farmax7777@yandex.ru>
2026-06-02 01:27:46 +0300

    Рефакторинг исходного кода и финализация структуры проекта

--- APItester/ApiTester.Core/Services/HttpClientService.cs
+++ APItester/ApiTester.Core/Services/HttpClientService.cs
@@ -18,17 +18,11 @@ public class HttpClientService : IDisposable
     {
         var httpRequest = new HttpRequestMessage(new HttpMethod(request.Method), request.Url);
 
-        // Add headers
         foreach (var header in request.Headers)
         {
-            if (!httpRequest.Headers.TryAddWithoutValidation(header.Key, header.Value))
-            {
-                // Some headers belong to content (like Content-Type)
-                // We will handle them when setting the body
-            }
+            httpRequest.Headers.TryAddWithoutValidation(header.Key, header.Value);
         }
 
-        // Add body
         if (!string.IsNullOrEmpty(request.Body))
         {
             var content = new StringContent(request.Body, Encoding.UTF8, request.BodyType ?? "application/json");
@@ -49,10 +43,8 @@ public class HttpClientService : IDisposable
                 ContentLength = response.Content.Headers.ContentLength ?? 0
             };
 
-            // Read body
             responseModel.Body = await response.Content.ReadAsStringAsync();
 
-            // Read headers
             foreach (var header in response.Headers)
             {
                 responseModel.Headers[header.Key] = string.Join(", ", header.Value);
